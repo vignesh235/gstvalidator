42111abc044fbf3cd7e152ace57ab3fcf07c7895
vignesh235 <vickyjai003@gmail.com>
2023-12-29 13:10:59 +0530

    Revert "add function to validate GST number"
    This reverts commit cd1f6fe7df50ae268105cdc7b520fef83aaabc3d.

--- lib/src/rendering.dart
+++ lib/src/rendering.dart
@@ -1,5 +1,6 @@
 part of gstvalidator;
 
+
 class GSTvalidator extends StatefulWidget {
   final String labelText;
 
@@ -36,8 +37,15 @@ class _GSTvalidatorState extends State<GSTvalidator> {
       autovalidateMode: widget.autovalidateMode,
       keyboardType: widget.keyboardType,
       validator: ((value) {
-        String res = validateGstinCheckDigit(widget.controller.text);
-        return res.isEmpty ? null : res;
+        final RegExp pattern = RegExp(
+            r"^[0-9]{2}[A-Z]{5}[0-9]{4}[A-Z]{1}[1-9A-Z]{1}Z[0-9A-Z]{1}$");
+        if (value == null || value.isEmpty) {
+          return '! Please enter value';
+        }
+        if (!pattern.hasMatch(value)) {
+          return '! Invalid GST format';
+        }
+        return null;
       }),
       maxLength: widget.maxLength,
       onChanged: (text) {
@@ -68,35 +76,9 @@ class _GSTvalidatorState extends State<GSTvalidator> {
       return 'Please enter a value';
     }
     if (!pattern.hasMatch(value)) {
-      return 'Invalid GST format';
+      return 'Invalid input format';
     }
     return null;
   }
-
-  String validateGstinCheckDigit(String gstin) {
-    var gstval = _validateInput(gstin);
-    if ((gstval ?? '').toString().isNotEmpty) {
-      return gstval.toString();
-    }
-    int factor = 1;
-    int total = 0;
-    String codePointChars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    int mod = codePointChars.length;
-    String inputChars = gstin.substring(0, gstin.length - 1);
-
-    for (int i = 0; i < inputChars.length; i++) {
-      String char = inputChars[i];
-      int digit = factor * codePointChars.indexOf(char);
-      digit = (digit ~/ mod) + (digit % mod);
-      total += digit;
-      factor = (factor == 1) ? 2 : 1;
-    }
-
-    if (gstin[gstin.length - 1] !=
-        codePointChars[((mod - (total % mod)) % mod)]) {
-      return "The GSTIN provided is not valid; \n It fails the check digit validation.";
-    }
-
-    return "";
-  }
 }
+
